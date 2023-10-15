1755ccf9cec3c7a4ec946ee7e165f393e393bda5
INRGI <120032162+INRGI@users.noreply.github.com>
2023-10-15 16:49:25 +0000

    Deploying to gh-pages from @ INRGI/goit-js-hw-10@90f8258ef9a115b302c4e40c6e3ff670244670ee 🚀

---
+++ index.00d6f628.js
@@ -0,0 +1,2 @@
+document.querySelector(".breed-select"),document.querySelector(".loader");
+//# sourceMappingURL=index.00d6f628.js.map
---
+++ index.aeae341d.js
@@ -0,0 +1,2 @@
+document.querySelector(".breed-select"),document.querySelector(".loader");
+//# sourceMappingURL=index.aeae341d.js.map
