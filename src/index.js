5702b69496bab8052f8ce97c31c78d30cb548665
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-16 18:35:49 +0200

    add apI key

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_16iRJIIL5P7x93BsQ6MezRPY1rNOI8N0Li1X9dy72xzZAucLkGS3P6ddgec2Lr3C";
+
+
 const selectBreedElement = document.querySelector('.breed-select');
 const selectLoader = document.querySelector('.loader');
