faffee3ae88b238da28356be8c87365fc46f1036
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-18 15:09:26 +0200

    add function fetchCatByBreed

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,11 +3,21 @@ const API_KEY = 'live_16iRJIIL5P7x93BsQ6MezRPY1rNOI8N0Li1X9dy72xzZAucLkGS3P6ddge
 
 export function fetchBreeds() {
     return fetch(`${BASIC_URL}/breeds?api_key=${API_KEY}`).then(
-        (response) => {
+        response => {
             if (!response.ok) {
                 throw new Erorr(response.statusText);
             }
 
+            return response.json();
+        }
+    )
+}
+export function fetchCatByBreed(breedId) {
+    return fetch(`${BASIC_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`).then(
+        response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
             return response.json();
         }
     )
