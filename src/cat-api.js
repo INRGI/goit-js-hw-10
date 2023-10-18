a37e2da5978e3a7667758e16cb77352758bbe9a0
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-18 15:03:43 +0200

    add function fetchBreeds

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,14 @@
-const ENDPOINT = 'https://api.thecatapi.com/v1/breeds';
+const BASIC_URL = 'https://api.thecatapi.com/v1';
 const API_KEY = 'live_16iRJIIL5P7x93BsQ6MezRPY1rNOI8N0Li1X9dy72xzZAucLkGS3P6ddgec2Lr3C';
 
-function fetchBreeds(){}
+export function fetchBreeds() {
+    return fetch(`${BASIC_URL}/breeds?api_key=${API_KEY}`).then(
+        (response) => {
+            if (!response.ok) {
+                throw new Erorr(response.statusText);
+            }
+
+            return response.json();
+        }
+    )
+}
