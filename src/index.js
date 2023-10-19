5f36667b96305b9ff385d7d9e24d614565252173
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-10-19 10:30:41 +0200

    add slim-select and other

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import axios from "axios";
 import Notiflix from 'notiflix';
-
-axios.defaults.headers.common["x-api-key"] = "live_16iRJIIL5P7x93BsQ6MezRPY1rNOI8N0Li1X9dy72xzZAucLkGS3P6ddgec2Lr3C";
-
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 const selectBreedElement = document.querySelector('.breed-select');
 const selectLoader = document.querySelector('.loader');
+const selectCatInfo = document.querySelector('.cat-info');
+const selectError = document.querySelector('.error');
